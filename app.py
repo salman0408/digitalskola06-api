7e90ce50506c8e999a8adb8123794962d4e8e571
salman0408 <kesatria.udara@gmail.com>
2022-04-19 21:47:35 +0700

    change#2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 
 def say_hello():
-    return jsonify({"text": "Semangat Saipullah"})
+    return jsonify({"text":"Semangat Saipullah"})
 
 if __name__ == "__main__":
     app.run(debug=True, port=8081)
